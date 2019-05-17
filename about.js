679271ae38efae694ac29da995e9f2b117d4c67f
StefanieBelfast <49611105+StefanieBelfast@users.noreply.github.com>
2019-05-17 13:56:12 +0100

    Add files via upload
    update, is running in terminal

--- about.js
+++ about.js
@@ -1,8 +1,10 @@
 describe('about', () => {
-    it('Visits the app Waste NI', () => {
-        cy.visit('http://52.209.112.20:8181/#/map')
-        cy.contains('Waste Stats NI')
-    }) 
+//    it('Visits the app Waste NI', () => {
+//        cy.visit('http://52.209.112.20:8181/#/map')
+//        cy.contains('Waste Stats NI')
+//    })
+//if you use a index before, you will not need it 
+ 
     it('existing text/items', () => {
         cy.contains('About').click()
         cy.get('h1').should('contain','About Page')
--- opendata.js
+++ opendata.js
@@ -1,8 +1,10 @@
 describe('opendata', () => {
-    it('Visits the app Waste NI', () => {
-        cy.visit('http://52.209.112.20:8181/#/map')
-        cy.contains('Waste Stats NI')
-    }) 
+//    it('Visits the app Waste NI', () => {
+//        cy.visit('http://52.209.112.20:8181/#/map')
+//        cy.contains('Waste Stats NI')
+//    })
+//if you use a index before, you will not need it 
+ 
     it('existing items', () => {
         cy.contains('Open Data').click()
         cy.get('h1').should('contain','Open Data')
